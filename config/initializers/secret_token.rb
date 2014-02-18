3135bcf68185b6a1292a0bacc3fcb822cd7e9a43
Derick Olson <olsonderick@gmail.com>
2014-02-17 19:46:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyCapsule::Application.config.secret_key_base = 'ba88bd02025dbb340d66768c37c15e09fa7e02360d6bde9baff0a8a82dc2d0a05c2adc11401336025b56ef9f8218ae6f4fdde23be51df163b7c5c5f9e1c07d0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyCapsule::Application.config.session_store :cookie_store, key: '_myCapsule_session'
