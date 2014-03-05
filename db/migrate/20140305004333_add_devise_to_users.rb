4f865d722211ba2563debfc371b6424bd3405851
Derick Olson <olsonderick@gmail.com>
2014-03-04 23:02:09 -0500

    heroku troubleshooting: email issue

--- db/migrate/20140305004333_add_devise_to_users.rb
+++ db/migrate/20140305004333_add_devise_to_users.rb
@@ -2,7 +2,6 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
@@ -35,7 +34,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
 
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps
+      t.timestamps
     end
 
     add_index :users, :email,                :unique => true
