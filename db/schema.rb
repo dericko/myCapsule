a329c15cbb8bb0e847c508c609d74b6abe91314c
Derick Olson <olsonderick@gmail.com>
2014-03-04 23:51:13 -0500

    heroku troubleshooting: email issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305032908) do
+ActiveRecord::Schema.define(version: 20140305045014) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
@@ -26,10 +26,7 @@ ActiveRecord::Schema.define(version: 20140305032908) do
   add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -39,9 +36,9 @@ ActiveRecord::Schema.define(version: 20140305032908) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "email"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
