58f08341ada89eb7433ecb2c2834e4169cad7cbe
Derick Olson <olsonderick@gmail.com>
2014-03-17 23:38:34 -0400

    HW7, Added validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :user
-  validates :body, length: {within: 0..500}
+  validates :body, length: {within: 2..500}
   validates :title, uniqueness: true
   validates :timer, numericality: {greater_than: 0}
 end
