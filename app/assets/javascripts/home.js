56772a97f5ab718651d8c4752c896318b0af9816
Derick Olson <olsonderick@gmail.com>
2014-05-09 16:22:27 -0400

    testing heroku

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function() {
+	$('#home-message').hide();
+	$('.img').hover( function() {
+		$(this).fadeTo(300, 0.5, function() {
+			$(this).fadeTo(300, 1);
+		});
+	});
+    $('.img-container').click( function() {
+    	$('.img-container').slideUp("slow", function() {
+    		$('.img-container').slideDown("slow", function() {
+    			$('#home-message').slideToggle("fast");
+    		});
+    		
+    	});
+    });
+});
